f129b817b8c28c4ce87fbc7e9a20d97eae754396
Edson Júnior <ebf.junin@gmail.com>
2017-08-25 20:44:30 -0300

    Webpack upgraded to version 2

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,21 +3,21 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    path: __dirname,
+    path: __dirname ,
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
